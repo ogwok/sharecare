a3bb7db64c7caa6caa07b66a2c3c38c29e92078b
ogwok <williamogwok10@gmail.com>
2022-08-18 02:10:12 +0300

    dynamic render bug fix

--- pages/details/[slug].js
+++ pages/details/[slug].js
@@ -1,85 +1,82 @@
-import React, {useState, useEffect} from 'react';
-import { Box, Typography, Button, Grid } from '@mui/material';
-import {AppBar} from '@mui/material';
-import FRBanner from '../../components/FRBanner';
-import FundRaiseDonate from '../../components/FundRaiseDonate';
-import DonationTabs from '../../components/DonationTabs';  
+import React, { useState, useEffect } from "react";
+import { Box, Typography, Button, Grid } from "@mui/material";
+import { AppBar } from "@mui/material";
+import FRBanner from "../../components/FRBanner";
+import FundRaiseDonate from "../../components/FundRaiseDonate";
+import DonationTabs from "../../components/DonationTabs";
+import axios from "axios";
 
-
-
-const Details = ({post}) => {
-console.log('gDFDSHVB', post)
+const Details = ({ post }) => {
+  console.log("gDFDSHVB", post.data.attributes);
   return (
     <AppBar position="static" sx={{ bgcolor: "#36c958" }}>
-        <Box sx={{bgcolor: '#ffffff'}}>
+      <Box sx={{ bgcolor: "#ffffff" }}>
         <FRBanner />
         <Box
-        sx={{
-            width: '100%',
-            height: 'auto',
-            backgroundColor: '#ffffff',
-            display: 'flex',
-            border: 'none'
-        }}
-        >
-        <Typography
-        gutterBottom
-        variant="h1"
-        sx={{
-            color: '#000000',
-            width: 'auto',
-            fontSize: {
-              md: 25,
-              xs: 20
-            },
-            ml:2 ,
-            mt:2,
-            justifyContent: 'flex-end',
-            fontWeight: 'bold'
+          sx={{
+            width: "100%",
+            height: "auto",
+            backgroundColor: "#ffffff",
+            display: "flex",
+            border: "none",
           }}
         >
-    {post.name}
-        </Typography>
+          <Typography
+            gutterBottom
+            variant="h1"
+            sx={{
+              color: "#000000",
+              width: "auto",
+              fontSize: {
+                md: 25,
+                xs: 20,
+              },
+              ml: 2,
+              mt: 2,
+              justifyContent: "flex-end",
+              fontWeight: "bold",
+            }}
+          >
+            {post.name}
+          </Typography>
         </Box>
-        <Box sx={{backgroundColor: '#ffffff'}}>
-            <FundRaiseDonate post={post}/>
-            <DonationTabs />
+        <Box sx={{ backgroundColor: "#ffffff" }}>
+          <FundRaiseDonate post={post} />
+          <DonationTabs />
         </Box>
-        </Box>
-    </AppBar> 
-  )
-}
+      </Box>
+    </AppBar>
+  );
+};
 export async function getStaticPaths() {
-    // Call an external API endpoint to get posts
-    const res = await fetch('http://localhost:4000/campaigns/')
-    const posts = await res.json()
-  
-    // Get the paths we want to pre-render based on posts
-    const paths = posts.map((post) => ({
-        params: {
-            slug: post._id ,
-        }
-     
-    }))
-  
-    // We'll pre-render only these paths at build time.
-    // { fallback: false } means other routes should 404.
-  
-    return { paths, fallback: 'blocking' }
-  }
-  
-  // This also gets called at build time
-  export async function getStaticProps({ params }) {
+  // Call an external API endpoint to get posts
 
-    
-    // params contains the post `id`.
-    // If the route is like /posts/1, then params.id is 1
-    const res = await fetch(`http://localhost:4000/campaigns/${params?.slug}`)
-    const post = await res.json()
-  
-    // Pass post data to the page via props
-    return { props: { post } }
-  }
+  const data2 = await axios.get("http://localhost:1337/api/campaigns");
 
-  
+  // Get the paths we want to pre-render based on posts
+  const paths = data2.data?.data.map((details) => ({
+    params: {
+      slug: details.id.toString(),
+    },
+  }));
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+
+  return { paths, fallback: "blocking" };
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+  // params contains the post `id`.
+  // If the route is like /posts/1, then params.id is 1
+  const res = await fetch(
+    `http://localhost:1337/api/campaigns/${params?.slug}`
+  );
+  const post = await res.json();
+
+  // Pass post data to the page via props
+  return { props: { post } };
+}
+
 export default Details;
--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,84 @@
-import React, {useState, useEffect} from 'react';
-import { Grid, Typography, Box, Button, AppBar} from '@mui/material';
-import Banner from '../components/Banner';
-import CampaignCard from './CampaignCard';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Grid, Typography, Box, Button, AppBar } from "@mui/material";
+import Banner from "../components/Banner";
+import CampaignCard from "./CampaignCard";
+import axios from "axios";
 
 export default function Home() {
-  const [campaigns, setCampaigns] = useState();
+  const [campaigns, setCampaigns] = useState([]);
+  // const [data2, setData2] = useState();
 
-  const fetchCampaigns = async() => {
-    const {data} = await axios.get('http://localhost:4000/campaigns')
-    setCampaigns(data);
-  }
-  console.log('gldsjgbsdgb', campaigns);
-  
+  const fetchCampaigns = async () => {
+    const { data } = await axios.get("http://localhost:1337/api/campaigns");
+    const data2 = await axios.get("http://localhost:1337/api/campaigns");
+    setCampaigns(data.data);
+    // setData2(data2);
+    console.log("hellllo", data2.data.data);
+  };
+  // console.log("gldsjgbsdgb", data2);
+
   useEffect(() => {
-      fetchCampaigns()
-  }, [])
+    fetchCampaigns();
+  }, []);
   return (
     <>
-    <AppBar position="static" sx={{ bgcolor: "#36c958" }}>
-      
-      <Banner />
-      <Box sx={{ bgcolor: "#ffffff" }}>
-        <Typography
-          gutterBottom
-          sx={{
-              color: '#000000',
+      <AppBar position="static" sx={{ bgcolor: "#36c958" }}>
+        <Banner />
+        <Box sx={{ bgcolor: "#ffffff" }}>
+          <Typography
+            gutterBottom
+            sx={{
+              color: "#000000",
               fontSize: {
                 md: 30,
-                xs: 25
+                xs: 25,
               },
               ml: 2,
-              mt: 2
+              mt: 2,
             }}
-        >
-        FundRaise For
-      </Typography>
-        <Box sx={{display: 'flex', justifyContent: 'flex-end', pr: 3, mt: -5}}>    
-              <Button variant="contained" 
-                sx={{
-                    boxShadow: 'none',
-                    textTransform: 'none',
-                    fontSize: 20,
-                    padding: '6px 12px',
-                    border: '1px solid',
-                    lineHeight: 1.5,
-                    backgroundColor: '#36C958',
-                    borderColor: '#2E2D33',
-                    '&:hover': {
-                      backgroundColor: '#000000',
-                      borderColor: '#0062cc',
-                      boxShadow: 'none',
-                    },
-                    }}
-              >
+          >
+            {/* {data2?.map((data) => (
+              <p>{data.id}</p>
+            ))} */}
+            FundRaise For
+          </Typography>
+          <Box
+            sx={{ display: "flex", justifyContent: "flex-end", pr: 3, mt: -5 }}
+          >
+            <Button
+              variant="contained"
+              sx={{
+                boxShadow: "none",
+                textTransform: "none",
+                fontSize: 20,
+                padding: "6px 12px",
+                border: "1px solid",
+                lineHeight: 1.5,
+                backgroundColor: "#36C958",
+                borderColor: "#2E2D33",
+                "&:hover": {
+                  backgroundColor: "#000000",
+                  borderColor: "#0062cc",
+                  boxShadow: "none",
+                },
+              }}
+            >
               See All
-              </Button>
-         </Box>
-      <Grid sx={{display: 'flex', flexWrap: 'wrap', alignItems: 'stretch', BackgroundColor: '#FFFFFF'}}>
-      {campaigns && campaigns?.map(data => (
-        <CampaignCard data={data}/>   
-      ))} 
-      </Grid>
-      </Box>
-    </AppBar>
-    </> 
-  )
+            </Button>
+          </Box>
+          <Grid
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              alignItems: "stretch",
+              BackgroundColor: "#FFFFFF",
+            }}
+          >
+            {campaigns &&
+              campaigns?.map((data) => <CampaignCard data={data} />)}
+          </Grid>
+        </Box>
+      </AppBar>
+    </>
+  );
 }
